d00b5fb240620e4affb0173ff79d505af37da2f4
James Cleveland <jc@blit.cc>
2021-06-18 15:43:13 +0100

    update to use OAI

--- src/static-web.ts
+++ src/static-web.ts
@@ -1,6 +1,7 @@
 import * as acm from '@aws-cdk/aws-certificatemanager';
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as iam from '@aws-cdk/aws-iam';
 import * as route53 from '@aws-cdk/aws-route53';
 import * as alias from '@aws-cdk/aws-route53-targets';
 import * as s3 from '@aws-cdk/aws-s3';
@@ -62,23 +63,40 @@ export class StaticWeb extends cdk.Construct {
   readonly aRecord?: route53.ARecord;
   readonly aaaaRecord?: route53.AaaaRecord;
   readonly deployment?: s3deploy.BucketDeployment;
+  readonly originAccessIdentity?: cloudfront.OriginAccessIdentity;
 
   constructor(scope: cdk.Construct, id: string, props: StaticWebProps) {
     super(scope, id);
+    this.originAccessIdentity = this.createOriginAccessIdentity();
     this.bucket = props.bucket ?? this.createBucket();
-    this.distribution = this.createDistribution(this.bucket, props);
+    this.distribution = this.createDistribution(this.bucket, this.originAccessIdentity, props);
+    const statement = this.createIAMStatement(this.bucket, this.originAccessIdentity);
+    this.bucket.addToResourcePolicy(statement);
     this.deployment = this.createDeployment(this.bucket, props, this.distribution);
-
     this.aRecord = this.createARecord(props, this.distribution);
     this.aaaaRecord = this.createAaaaRecord(props, this.distribution);
   }
 
   private createBucket(): s3.Bucket {
-    return new s3.Bucket(this, `Bucket`);
+    return new s3.Bucket(this, `Bucket`, {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+  }
+
+  private createIAMStatement(bucket: s3.IBucket, originAccessIdentity: cloudfront.OriginAccessIdentity) {
+    const statement = new iam.PolicyStatement();
+    statement.addActions('s3:GetBucket*');
+    statement.addActions('s3:GetObject*');
+    statement.addActions('s3:List*');
+    statement.addResources(bucket.bucketArn);
+    statement.addResources(`${bucket.bucketArn}/*`);
+    statement.addCanonicalUserPrincipal(originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId);
+    return statement;
   }
 
   private createDistribution(
     bucket: s3.IBucket,
+    originAccessIdentity: cloudfront.OriginAccessIdentity,
     { distributionProps, behaviourOptions, isSPA, certificate, recordName, zone }: StaticWebProps,
   ) {
     const errorResponses = [];
@@ -102,7 +120,9 @@ export class StaticWeb extends cdk.Construct {
 
     return new cloudfront.Distribution(this, `Distribution`, {
       defaultBehavior: {
-        origin: new origins.S3Origin(bucket),
+        origin: new origins.S3Origin(bucket, {
+          originAccessIdentity,
+        }),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         ...behaviourOptions,
       },
@@ -112,6 +132,10 @@ export class StaticWeb extends cdk.Construct {
       certificate,
       ...distributionProps,
     });
+  }
+
+  private createOriginAccessIdentity() {
+    return new cloudfront.OriginAccessIdentity(this, `OriginAccessIdentity`);
   }
 
   private createDeployment(
