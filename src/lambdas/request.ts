014383f9fafcf3c10fbe2a258845feaf58bbb89c
James Cleveland <jc@blit.cc>
2021-06-25 11:25:59 +0100

    add error handling

--- src/lambdas/request.ts
+++ src/lambdas/request.ts
@@ -2,17 +2,25 @@ import type { CloudFrontRequestEvent, CloudFrontRequestResult } from 'aws-lambda
 import * as path from 'path';
 
 export async function handler(event: CloudFrontRequestEvent): Promise<CloudFrontRequestResult> {
-  const request = event.Records[0].cf.request;
-  const extension = path.extname(request.uri);
+  try {
+    const request = event.Records[0].cf.request;
+    const extension = path.extname(request.uri);
 
-  request.headers['x-original-uri'] = [{ key: 'x-original-uri', value: request.uri }];
+    request.headers['x-original-uri'] = [{ key: 'x-original-uri', value: request.uri }];
 
-  if (!extension) {
-    request.headers['x-rewritten'] = [{ key: 'x-rewritten', value: 'true' }];
-    request.uri = `${request.uri.replace(/\/$/, '')}/index.html`;
+    if (!extension) {
+      request.headers['x-rewritten'] = [{ key: 'x-rewritten', value: 'true' }];
+      request.uri = `${request.uri.replace(/\/$/, '')}/index.html`;
 
-    console.log('updated uri to', request.uri);
+      console.log('updated uri to', request.uri);
+    }
+
+    return request;
+  } catch (err) {
+    return {
+      status: '500',
+      statusDescription: 'Internal Server Error',
+      body: `${err}`,
+    };
   }
-
-  return request;
 }
