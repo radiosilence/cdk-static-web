d534df4be6c99429b70500e0ab396dbbf3738d97
James Cleveland <jc@blit.cc>
2024-05-23 20:30:16 +0100

    go back to cjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -4,7 +4,7 @@ export default esbuild.build({
   entryPoints: ['src/index.ts', 'src/lambdas/request/index.ts', 'src/lambdas/response/index.ts'],
   bundle: true,
   minify: true,
-  format: 'esm',
+  format: 'cjs',
   packages: 'external',
   outdir: 'dist',
   target: 'es2020',
