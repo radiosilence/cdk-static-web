a04d47f5344fd86a28534c6cd5c960f4f1051a60
James Cleveland <jc@blit.cc>
2024-05-23 20:17:57 +0100

    package lambdas with esbuild also

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,11 +1,11 @@
 import esbuild from 'esbuild';
 
 export default esbuild.build({
-  entryPoints: ['src/index.mts'],
+  entryPoints: ['src/index.ts', 'src/lambdas/request/index.ts', 'src/lambdas/response/index.ts'],
   bundle: true,
   minify: true,
   format: 'esm',
   packages: 'external',
-  outfile: 'dist/index.min.mjs',
+  outdir: 'dist',
   target: 'es2020',
 });
